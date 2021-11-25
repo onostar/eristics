80861124bab43fabed18e6534df4a8b9c384a108
onostar <onostarkels@gmail.com>
2021-11-25 06:09:10 +0100

    added my database

--- eristics_eristics.sql
+++ eristics_eristics.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2021 at 02:18 PM
+-- Generation Time: Nov 25, 2021 at 06:04 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.20
 
